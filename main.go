945e3d0c1d569aa3935ad8a688a5999d8f6a0def
ydzly <ydglitch@gmail.com>
2024-07-17 21:16:05 -0400

    upd: main

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	
 	"net/http"
+
+	"github.com/dglitxh/velvet/common"
 	"github.com/dglitxh/velvet/util"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	// "github.com/spf13/viper"
 )
 
 func main() {
-	// viper.SetConfigFile("./common/envs/.env")
-	// viper.ReadInConfig()
+	common.LoadConfig()
+	viper.SetConfigFile("./.env")
+	viper.ReadInConfig()
 
-
+	
 	port := ":8000"
 	r := gin.Default()
 	
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"status": "We are LIVE!!!"})
 	})
+	common.ConnectDb()
+	
+	util.Log(util.DefaultLogFile, "get me litt")
 
 	r.Run(port)
 
-	util.Log(util.DefaultLogFile, "get me litt")
+	
 }
